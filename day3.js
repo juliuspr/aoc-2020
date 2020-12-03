eb021cbc0e58f90e2f74f42feb7e4b98f2efa587
Julius Pranevicius <juliuspr@users.noreply.github.com>
2020-12-03 21:15:47 +0100

    Update day3.js

--- day3.js
+++ day3.js
@@ -1,13 +1,13 @@
-let map = data.split('\n')
+// let data = 'INPUT DATA';
+let map = data.split('\n');
 
 let isTree = (line, x) => line[x] === '#'
 
 function countTrees(map, [slopeX, slopeY], [currentX,currentY] = [0,0]) {
     let numberOfTrees = 0;
 
-    for (let y = currentY; y < map.length; y = y + slopeY) {
-        if(isTree(map[y], currentX % 31)) numberOfTrees++;
-
+    for (currentY; currentY < map.length; currentY += slopeY) {
+        if(isTree(map[currentY], currentX % map[currentY].length)) numberOfTrees++;
         currentX += slopeX;
     }
     return numberOfTrees;
@@ -20,8 +20,8 @@ const slopes = [
     [7,1],
     [1,2]];
 
-let part1 = countTrees(map, [3,1])
+let part1 = countTrees(map, [3,1]);
 
-let part2 = slopes.reduce((acc, current) => acc * countTrees(map, current), 1)
+let part2 = slopes.reduce((acc, current) => acc * countTrees(map, current), 1);
 
 console.log(part1, part2);
